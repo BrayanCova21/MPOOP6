9e78a9040ef780594c506e61fb5b54b9f9b2f548
Brayan-AldairAl <>
2021-10-21 19:26:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+/**
+* @autor Brayan Cova
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+
+  }
+  
+
+/**
+* @param a Primer entero de la ejecucion 
+* @param b Segundo entero de la suma
+* @return La suma de los enteros a más b
+*/
+
+
+public static int suma(int a, int b)
+{
+  return a+b;
+}
+
+}
+
